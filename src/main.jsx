e0972918e1bb3433aeddda9ced6e92bdf04c623a
Diana Kovacheva <dmarinova@outlook.com>
2024-04-18 16:35:38 +0200

    Add router

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 import "./index.css";
 
 import "@fontsource/inter";
 
+import App from "./App.jsx";
+import Login from "./components/login/login.jsx";
+import PageNotFound from "./components/page-not-found/PageNotFound.jsx";
+
+const router = createBrowserRouter([
+  { path: "/", element: <App /> },
+  { path: "/login", element: <Login /> },
+  { path: "*", element: <PageNotFound /> },
+]);
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
