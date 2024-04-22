7ada3bb29a3df60976d86b5c5e707307b34f6a0b
Diana Kovacheva <dmarinova@outlook.com>
2024-04-22 12:20:15 +0200

    Create SearchField component

---
+++ src/components/search-field/SearchField.jsx
@@ -0,0 +1,28 @@
+import * as React from "react";
+import FormControl from "@mui/joy/FormControl";
+import FormLabel from "@mui/joy/FormLabel";
+import Stack from "@mui/joy/Stack";
+import Autocomplete from "@mui/joy/Autocomplete";
+
+export default function SearchField() {
+  return (
+    <Stack spacing={2} sx={{ width: "60%" }} order={3}>
+      <FormControl id="free-solo-2-demo">
+        <Autocomplete
+          placeholder="Search anything..."
+          type="search"
+          freeSolo
+          disableClearable
+          options={top100Films.map((option) => option.title)}
+        />
+      </FormControl>
+    </Stack>
+  );
+}
+
+// Top 100 films as rated by IMDb users. http://www.imdb.com/chart/top
+const top100Films = [
+  { title: "The Shawshank Redemption", year: 1994 },
+  { title: "The Godfather", year: 1972 },
+  { title: "The Godfather: Part II", year: 1974 },
+];
