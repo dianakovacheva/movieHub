9ddd457884c0d9974cea4767930ab44f3279fc92
Diana Kovacheva <dmarinova@outlook.com>
2024-04-23 18:10:27 +0200

    Add SignInButton component

--- src/components/sign-in-button/SignInButton.jsx
+++ src/components/sign-in-button/SignInButton.jsx
@@ -2,18 +2,16 @@ import * as React from "react";
 import Box from "@mui/joy/Box";
 import Button from "@mui/joy/Button";
 
+import { Link } from "react-router-dom";
+
+import SignInButtonCSS from "./SignInButtonCSS.module.css";
+
 export default function SignInButton() {
   return (
-    <Box
-      sx={{
-        display: "flex",
-        gap: 2,
-        flexWrap: "wrap",
-        textWrap: "nowrap",
-      }}
-      order={6}
-    >
-      <Button color="none">Sign in</Button>
+    <Box className={SignInButtonCSS.container}>
+      <Link to="/sign-in">
+        <Button color="none">Sign In</Button>
+      </Link>
     </Box>
   );
 }
