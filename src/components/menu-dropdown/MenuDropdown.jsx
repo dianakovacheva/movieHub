c19712b579f96b7ad211ca6f04bb4bb29056ca31
Diana Kovacheva <dmarinova@outlook.com>
2024-04-23 18:08:18 +0200

    Add nested menu items

--- src/components/menu-dropdown/MenuDropdown.jsx
+++ src/components/menu-dropdown/MenuDropdown.jsx
@@ -8,11 +8,14 @@ import ListItemDecorator from "@mui/joy/ListItemDecorator";
 import LocalMoviesIcon from "@mui/icons-material/LocalMovies";
 import TvIcon from "@mui/icons-material/Tv";
 import GroupIcon from "@mui/icons-material/Group";
+import List from "@mui/joy/List";
+import ListItem from "@mui/joy/ListItem";
+import { Link } from "react-router-dom";
 
 import MenuDropdownCSS from "./MenuDropdownCSS.module.css";
 
 export default function MenuDropdown() {
-  const [selectedIndex, setSelectedIndex] = React.useState(1);
+  const [selectedIndex, setSelectedIndex] = React.useState(0);
 
   const createHandleClose = (index) => () => {
     if (typeof index === "number") {
@@ -30,30 +33,112 @@ export default function MenuDropdown() {
         Menu
       </MenuButton>
       <Menu>
-        <MenuItem
-          {...(selectedIndex === 0 && {
-            selected: true,
-            variant: "soft",
-          })}
-          onClick={createHandleClose(0)}
-        >
-          <ListItemDecorator>
-            <LocalMoviesIcon />
-          </ListItemDecorator>
-          Movies
-        </MenuItem>
-        <MenuItem selected={selectedIndex === 1} onClick={createHandleClose(1)}>
-          <ListItemDecorator>
-            <TvIcon />
-          </ListItemDecorator>
-          TV Shows
-        </MenuItem>
-        <MenuItem selected={selectedIndex === 2} onClick={createHandleClose(2)}>
-          <ListItemDecorator>
-            <GroupIcon />
-          </ListItemDecorator>
-          Celebs
-        </MenuItem>
+        <List>
+          {/* Start Movies */}
+          <ListItem
+            selected={selectedIndex === 1}
+            onClick={createHandleClose(1)}
+          >
+            <ListItemDecorator>
+              <LocalMoviesIcon />
+            </ListItemDecorator>
+            Movies
+          </ListItem>
+          <MenuItem
+            {...(selectedIndex === 1 && { selected: true, variant: "soft" })}
+            onClick={createHandleClose(1)}
+          >
+            <Link to="/popular-movies">Popular Movies</Link>
+          </MenuItem>
+          <MenuItem
+            {...(selectedIndex === 1 && { selected: true, variant: "soft" })}
+            onClick={createHandleClose(1)}
+          >
+            <Link to="/now-playing-movies">Now Playing</Link>
+          </MenuItem>
+          <MenuItem
+            {...(selectedIndex === 1 && { selected: true, variant: "soft" })}
+            onClick={createHandleClose(1)}
+          >
+            <Link to="/upcoming-movies">Upcoming</Link>
+          </MenuItem>
+          <MenuItem
+            {...(selectedIndex === 1 && { selected: true, variant: "soft" })}
+            onClick={createHandleClose(1)}
+          >
+            <Link to="/top-rated-movies">Top Rated</Link>
+          </MenuItem>
+          {/* End Movies */}
+
+          {/* Start TV Shows */}
+          <ListItem
+            selected={selectedIndex === 2}
+            onClick={createHandleClose(2)}
+          >
+            <ListItemDecorator>
+              <TvIcon />
+            </ListItemDecorator>
+            TV Shows
+          </ListItem>
+          <MenuItem
+            {...(selectedIndex === 2 && {
+              selected: true,
+              variant: "soft",
+            })}
+            onClick={createHandleClose(2)}
+          >
+            <Link to="/popular-shows">Popular</Link>
+          </MenuItem>
+          <MenuItem
+            {...(selectedIndex === 2 && {
+              selected: true,
+              variant: "soft",
+            })}
+            onClick={createHandleClose(2)}
+          >
+            <Link to="/airing-today-shows">Airing Today</Link>
+          </MenuItem>
+          <MenuItem
+            {...(selectedIndex === 2 && {
+              selected: true,
+              variant: "soft",
+            })}
+            onClick={createHandleClose(2)}
+          >
+            <Link to="/on-tv-shows">On TV</Link>
+          </MenuItem>
+          <MenuItem
+            {...(selectedIndex === 2 && {
+              selected: true,
+              variant: "soft",
+            })}
+            onClick={createHandleClose(2)}
+          >
+            <Link to="/top-rated-shows">Top Rated</Link>
+          </MenuItem>
+          {/* End TV Shows */}
+
+          {/* Start Celebs */}
+          <ListItem
+            selected={selectedIndex === 3}
+            onClick={createHandleClose(3)}
+          >
+            <ListItemDecorator>
+              <GroupIcon />
+            </ListItemDecorator>
+            Celebs
+          </ListItem>
+          <MenuItem
+            {...(selectedIndex === 3 && {
+              selected: true,
+              variant: "soft",
+            })}
+            onClick={createHandleClose(3)}
+          >
+            <Link to="/most-popular-celebs">Most Popular Celebs</Link>
+          </MenuItem>
+          {/* End Celebs */}
+        </List>
       </Menu>
     </Dropdown>
   );
