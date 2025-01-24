56026e3137a4ef434bb8904d32585910b0ea8799
Diana Kovacheva <dmarinova@outlook.com>
2025-01-24 09:37:16 +0100

    Center the content

--- movie-hub/app/layout.tsx
+++ movie-hub/app/layout.tsx
@@ -23,9 +23,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" data-theme="dark">
+    <html lang="en">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </head>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased max-w-lvw align-baseline flex flex-col flex-wrap justify-center content-center`}
       >
         {children}
       </body>
